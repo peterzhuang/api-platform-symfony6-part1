76d31cb4e436221657178c4c5d8d633c6186b4e1
Yuchuan <yuchuan@example.com>
2022-12-15 16:37:58 -0500

    Setup logic on session-based authentication success

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,21 +2,26 @@
 
 namespace App\Controller;
 
+use ApiPlatform\Api\IriConverterInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login', methods: ['POST'])]
-    public function login()
+    public function login(IriConverterInterface $iriConverter)
     {
         if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
             return $this->json([
                 'error' => 'Invalid login request: check that the Content-Type header is "application/json"'
             ], 400);
         }
-        return $this->json([
-            'user' => $this->getUser() ? $this->getUser()->getId() : null,
+        // return $this->json([
+        //     'user' => $this->getUser() ? $this->getUser()->getId() : null,
+        // ]);
+        return new Response(null, 204, [
+            'Location' => $iriConverter->getIriFromResource($this->getUser())
         ]);
     }
 }
