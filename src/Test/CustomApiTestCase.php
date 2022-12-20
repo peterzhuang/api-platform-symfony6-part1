ec8efc12b8a04a00dc94e6739ae10b433f087b5d
Yuchuan <yuchuan@example.com>
2022-12-20 18:28:34 -0500

    Refactor custom API testcase base class

---
+++ src/Test/CustomApiTestCase.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Test;
+
+use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use ApiPlatform\Symfony\Bundle\Test\Client;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class CustomApiTestCase extends ApiTestCase
+{
+    protected function createUser(string $email, string $password): User
+    {
+        $user = new User();
+        $user->setEmail($email);
+        $user->setUsername(substr($email, 0, strpos($email, '@')));
+        
+        // $user->setPassword($password);
+        // $encoded = static::getContainer()->get(UserPasswordHasherInterface::class)->hashPassword($user, $password);
+        $encoded = static::getContainer()->get('test.security.user_password_hasher')->hashPassword($user, $password);
+        $user->setPassword($encoded);
+ 
+        $em = static::getContainer()->get(EntityManagerInterface::class);
+        $em->persist($user);
+        $em->flush();
+
+        return $user;
+    }
+
+    protected function logIn(Client $client, string $email, string $password)
+    {
+        $client->request('POST', '/login', [
+            // 'headers' => ['Content-Type' => 'application/json'],
+            'json' => [
+                'email' => $email,
+                'password' => $password,
+            ],
+           ]);
+    
+           $this->assertResponseStatusCodeSame(204);
+    }
+
+    protected function createUserAndLogIn(Client $client, string $email, string $password): User
+    {
+        $user = $this->createUser($email, $password);
+        $this->logIn($client, $email, $password);
+        return $user;
+    }
+}
--- tests/Functional/CheeseListingResourceTest.php
+++ tests/Functional/CheeseListingResourceTest.php
@@ -4,10 +4,11 @@ namespace App\Tests\Functional;
 
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
 use App\Entity\User;
+use App\Test\CustomApiTestCase;
 use Doctrine\ORM\EntityManagerInterface;
 use Hautelook\AliceBundle\PhpUnit\ReloadDatabaseTrait;
 
-class CheeseListingResourceTest extends ApiTestCase
+class CheeseListingResourceTest extends CustomApiTestCase
 {
     use ReloadDatabaseTrait;
 
@@ -16,32 +17,43 @@ class CheeseListingResourceTest extends ApiTestCase
        $client = self::createClient();
 
        $client->request('POST', '/api/cheeses', [
-        'headers' => ['Content-Type' => 'application/json'],
+        // 'headers' => ['Content-Type' => 'application/json'],
         'json' => [],
        ]);
 
        $this->assertResponseStatusCodeSame(401);
 
-       $user = new User();
-       $user->setEmail('cheeseplease@example.com');
-       $user->setUsername('cheeseplease');
-       $user->setPassword('$2y$13$fsbWP0wKvy0f4aAhlhkMG.Xbc6DxzpblQiUXYdTB4yucbps0GeYjK');
+    //    $user = new User();
+    //    $user->setEmail('cheeseplease@example.com');
+    //    $user->setUsername('cheeseplease');
+    //    $user->setPassword('$2y$13$fsbWP0wKvy0f4aAhlhkMG.Xbc6DxzpblQiUXYdTB4yucbps0GeYjK');
 
-       $em = static::getContainer()->get(EntityManagerInterface::class);
-       $em->persist($user);
-       $em->flush();
+    //    $em = static::getContainer()->get(EntityManagerInterface::class);
+    //    $em->persist($user);
+    //    $em->flush();
+    
+        // $this->createUser('cheeseplease@example.com', 'foo');
 
-       $client->request('POST', '/login', [
-        'headers' => ['Content-Type' => 'application/json'],
-        'json' => [
-            'email' => 'cheeseplease@example.com',
-            'password' => 'foo',
-        ],
-       ]);
+    //    $client->request('POST', '/login', [
+    //     'headers' => ['Content-Type' => 'application/json'],
+    //     'json' => [
+    //         'email' => 'cheeseplease@example.com',
+    //         'password' => 'foo',
+    //     ],
+    //    ]);
 
-       $this->assertResponseStatusCodeSame(204);
+    //    $this->assertResponseStatusCodeSame(204);
 
+        // $this->logIn($client, 'cheeseplease@example.com', 'foo');
 
+        $this->createUserAndLogIn($client, 'cheeseplease@example.com', 'foo');
+
+        $client->request('POST', '/api/cheeses', [
+            // 'headers' => ['Content-Type' => 'application/json'],
+            'json' => [],
+           ]);
+    
+           $this->assertResponseStatusCodeSame(400);
 
     }
 }
