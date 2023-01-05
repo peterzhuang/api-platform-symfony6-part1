eeeaab28aa0ff62096ae07270bde1bb745d6de85
Yuchuan <yuchuan@example.com>
2023-01-04 19:42:58 -0500

    fix User Normalizer to see phoneNumber for user's own record and config isMe custom field via normalizer

--- src/Serializer/Normalizer/UserNormalizer.php
+++ src/Serializer/Normalizer/UserNormalizer.php
@@ -4,6 +4,7 @@ namespace App\Serializer\Normalizer;
 
 use App\Entity\User;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Serializer\Normalizer\CacheableSupportsMethodInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
@@ -23,12 +24,17 @@ class UserNormalizer implements NormalizerInterface, CacheableSupportsMethodInte
 
     private const ALREADY_CALLED = 'USER_NORMALIZER_ALREADY_CALLED';
 
+    public function __construct(private Security $security)
+    {
+    }
+
     /**
      * @param User $object
      */
     public function normalize($object, string $format = null, array $context = []): array
     {
-        if ($this->userIsOwner($object)){
+        $isOwner = $this->userIsOwner($object);
+        if ($isOwner){
             $context['groups'][] = 'owner:read';
         }
 
@@ -37,6 +43,7 @@ class UserNormalizer implements NormalizerInterface, CacheableSupportsMethodInte
         $data = $this->normalizer->normalize($object, $format, $context);
 
         // TODO: add, edit, or delete some data
+        $data['isMe'] = $isOwner;
 
         return $data;
     }
@@ -57,14 +64,14 @@ class UserNormalizer implements NormalizerInterface, CacheableSupportsMethodInte
 
     private function userIsOwner(User $user): bool
     {
-        // /** @var User|null $authenticatedUser */
-        // $authenticatedUser = $this->security->getUser();
+        /** @var User|null $authenticatedUser */
+        $authenticatedUser = $this->security->getUser();
 
-        // if (!$authenticatedUser) {
-        //     return false;
-        // }
+        if (!$authenticatedUser) {
+            return false;
+        }
 
-        // return $authenticatedUser->getEmail() === $user->getEmail();
-        return mt_rand(0, 10) > 5;
+        return $authenticatedUser->getEmail() === $user->getEmail();
+        // return mt_rand(0, 10) > 5;
     }
 }
